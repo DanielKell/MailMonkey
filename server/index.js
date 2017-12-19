02a41cd01c2fefa1e0451087c722194fbaced736
DanielKell <danielkell.ubc@gmail.com>
2017-12-19 13:24:15 -0800

    First Commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express(); //Generates a new application
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5001; 
+//When Heroku runs our app, it injects
+//environment variables; set in the underlying runtime. 
+//We want to pick these up at the appropriate time and use them.
+app.listen(PORT);
