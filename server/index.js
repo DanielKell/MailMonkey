ddf7ccdbf1748f0d44080da7f2bb27a410990de0
DanielKell <danielkell.ubc@gmail.com>
2018-01-04 16:50:26 -0500

    Added basic Google OAuth Support

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express(); //Generates a new application
 
-app.get('/', (req, res) => {
-    res.send({bye: 'there' });
-});
+//How to authenticate users inside of our application
+//Setting up passport with the ID, secret, and callback URL
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+        console.log(accessToken);
+    })
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }
+))
+//Scope = what we want to have access to in google
 
 const PORT = process.env.PORT || 5001; 
 //When Heroku runs our app, it injects
