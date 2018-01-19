690620c894ff0d52fe404fd946eb105ca636f9e5
DanielKell <danielkell.ubc@gmail.com>
2018-01-19 11:22:52 +1100

    Add basic AuthReducer

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import Reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(Reducers, {}, applyMiddleware());
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
