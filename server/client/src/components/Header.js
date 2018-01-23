5cb3239c2fcf6277841180c1da40d3a6c09b46c7
DanielKell <danielkell.ubc@gmail.com>
2018-01-23 22:22:54 +1100

    App component setup with fetch user dispatch

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
         <nav>
-            <div class="nav-wrapper">
+            <div className="nav-wrapper">
             <a className="left brand-logo">MailMonkey</a>
             <ul id="nav-mobile" className="right">
                 <li><a>Login with Google</a></li>
