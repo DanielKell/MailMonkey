899b64d33ab16d350b37c71f036ffb3e5d9a9c00
DanielKell <danielkell.ubc@gmail.com>
2018-01-23 23:30:05 +1100

    Conditionally render login button

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return ;
+            case false:
+                return (
+                    <li><a href="/auth/google">Login With Google </a></li>
+                )
+            default: 
+                return <li><a>Logout</a></li>
+        }
+    }
+
     render() {
         return (
         <nav>
             <div className="nav-wrapper">
             <a className="left brand-logo">MailMonkey</a>
             <ul id="nav-mobile" className="right">
-                <li><a>Login with Google</a></li>
+                {this.renderContent()}
             </ul>
             </div>
         </nav>
@@ -15,4 +29,8 @@ class Header extends Component {
     }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect (mapStateToProps)(Header);
