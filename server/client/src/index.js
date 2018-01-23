dd095a213125c6355a3ef150c03b594fb4461809
DanielKell <danielkell.ubc@gmail.com>
2018-01-23 14:58:01 +1100

    Added Axios and Thunk for action creator

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,10 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import Reducers from './reducers';
 
-const store = createStore(Reducers, {}, applyMiddleware());
+
+
+const store = createStore(Reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
