425827a72dcface8e6f0be499c8c76c2fb628d19
DanielKell <danielkell.ubc@gmail.com>
2018-01-23 14:25:49 +1100

    Styling for header and container

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
+        <nav>
+            <div class="nav-wrapper">
+            <a className="left brand-logo">MailMonkey</a>
+            <ul id="nav-mobile" className="right">
+                <li><a>Login with Google</a></li>
+            </ul>
             </div>
+        </nav>
         );
     }
 }
