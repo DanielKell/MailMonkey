4bdef36f27f63ad72a5a1e836b22b0d631057beb
DanielKell <danielkell.ubc@gmail.com>
2018-01-25 21:52:21 +1100

    Restyled Credits button

--- server/client/src/components/StripePayments.js
+++ server/client/src/components/StripePayments.js
@@ -6,10 +6,16 @@ class Payments extends Component {
 
         return (
             <StripeCheckout 
+                name="MailMonkey"
+                description="Receive 5 Email Credits for $5"
                 amount={500} //in cents
                 token={token => console.log(token)} //callback fn after receiving token
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">
+                    Add Credits
+                </button>
+            </StripeCheckout>
         );
     }
 }
