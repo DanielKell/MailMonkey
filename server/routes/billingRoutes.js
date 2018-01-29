a3eaa0bee34de95adfe34c0d02750dfabc6127d2
DanielKell <danielkell.ubc@gmail.com>
2018-01-29 22:33:14 +1000

    Fixed bug if user is not logged in and tries to access /api/stripe

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -5,6 +5,12 @@ const stripe = require('stripe')(
 
 module.exports = app => {
     app.post('/api/stripe', async (req, res) => {
+
+        if (!req.user) {
+            return res.status(401).send({ error: 'You must log in' });
+            //You aren't logged in
+        }
+
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
