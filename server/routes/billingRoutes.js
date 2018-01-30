9ff19ce0b0df7edb7ac8cc547539db34e2cd663d
DanielKell <danielkell.ubc@gmail.com>
2018-01-30 22:29:27 +1000

    Moved requireLogin function

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,14 +2,10 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(
     keys.stripeSecretKey
 );
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
-
-        if (!req.user) {
-            return res.status(401).send({ error: 'You must log in' });
-            //You aren't logged in
-        }
+    app.post('/api/stripe', requireLogin, async (req, res) => {
 
         const charge = await stripe.charges.create({
             amount: 500,
