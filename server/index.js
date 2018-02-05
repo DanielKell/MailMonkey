74f353060c4626eed11bca73a083a131bb2caf9b
DanielKell <danielkell.ubc@gmail.com>
2018-02-05 11:16:15 +1100

    Resolved routes for express

--- server/index.js
+++ server/index.js
@@ -28,5 +28,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    //Any get requests come in and isn't defined, look in client/build
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 5001; 
 app.listen(PORT);
