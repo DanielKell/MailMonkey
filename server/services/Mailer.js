df3c8ebf205a2dafd54bf96144e38e546862dcbe
DanielKell <danielkell.ubc@gmail.com>
2018-02-09 10:18:06 -0800

    Mailer Class setup

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -3,6 +3,33 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
+            //Sendgrid uses helper to format sections
+        this.from_email = new helper.Email('no-reply@mailmonkey.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients
+    }
+
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        })
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const addClickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
 
 }
 
