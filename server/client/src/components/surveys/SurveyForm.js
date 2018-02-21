17c93ea3e7bcb87d2dac34f2ac024ac9060e12e3
DanielKell <danielkell.ubc@gmail.com>
2018-02-21 14:26:44 -0800

    Cancel button and button styling

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 const FIELDS = [
     { label: 'Survey Title', name: 'title'},
@@ -16,7 +17,7 @@ class SurveyForm extends Component {
     renderFields() {
         return _.map(FIELDS, ({label, name}) => {
             return (
-            <Field component={SurveyField} type="text" label={label} name={name} /> 
+            <Field key={name} component={SurveyField} type="text" label={label} name={name} /> 
             )}
         )}
 
@@ -26,7 +27,13 @@ class SurveyForm extends Component {
                 <form //arrow function called on submit
                 onSubmit={this.props.handleSubmit(values => console.log(values))}>
                 {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button className="teal btn-flat right white-text" type="submit">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         )
