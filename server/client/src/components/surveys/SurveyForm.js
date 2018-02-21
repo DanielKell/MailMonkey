f94cc872a37b0d86c33e65e788bc6027b3ee4d51
DanielKell <danielkell.ubc@gmail.com>
2018-02-21 14:10:28 -0800

    Map over label fields and output

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import _ from 'lodash';
+
+const FIELDS = [
+    { label: 'Survey Title', name: 'title'},
+    { label: 'Subject Line', name: 'subject'},
+    { label: 'Email Body', name: 'body'},
+    { label: 'Recipient List', name: 'emails'}
+];
+
 
 class SurveyForm extends Component {
 
     renderFields() {
-        return (
-            <div>
-                <Field
-                    type="text"
-                    name="title"
-                    component={SurveyField}
-                />
-            </div>
-        )
-    }
-
+        return _.map(FIELDS, ({label, name}) => {
+            return (
+            <Field component={SurveyField} type="text" label={label} name={name} /> 
+            )}
+        )}
 
     render() {
         return (
