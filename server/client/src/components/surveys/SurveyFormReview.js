0e5bac0ea81ffdd50ac51b92ac51580c60e8189d
DanielKell <danielkell.ubc@gmail.com>
2018-02-22 13:42:39 -0800

    Draft up review page

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const SurveyReview = () => {
+const SurveyReview = ({ onCancel, formValues }) => {
     return (
         <div>
             <h5>Please confirm your inputs</h5>
+            <div>
+                <div>
+                    <label>Survey Title</label>
+                    <div>{formValues.title}</div>
+                </div>
+                <div>
+                    <label>Subject Line</label>
+                    <div>{formValues.subject}</div>
+                </div>
+                <div>
+                    <label>Email Body</label>
+                    <div>{formValues.body}</div>
+                </div>
+                <div>
+                    <label>Emails</label>
+                    <div>{formValues.emails}</div>
+                </div>
+            </div>
+            <button 
+                className="red darken-3 btn-flat" 
+                onClick={onCancel}    
+            >
+                Back
+            </button>
         </div>
     )
 }
 
-export default SurveyReview
+function mapStateToProps(state) {
+    return {
+        formValues: state.form.surveyForm.values
+    }
+}
+
+export default connect(mapStateToProps)(SurveyReview)
